eccaa43a030a7ab9da83a809fd416c66d4bd8184
brendarrJesus <brendarafah681@gmail.com>
2025-05-18 21:01:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationstate)
+        {
+            var window = base.CreateWindow(activationstate);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+        }
+
+    }
+}
